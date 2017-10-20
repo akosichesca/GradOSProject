a3ba1df85f207708e53b959945e79952335b4ab3
Nathan Vance <natervance@gmail.com>
2017-10-20 14:15:38 -0400

    reorganized code

---
+++ cryptography.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+import time
+import sys
+import os
+import subprocess
+from Crypto.Cipher import AES, DES3, ARC4
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
+
+message= ""
+
+def read(filename):
+    global message
+    with open(filename, 'rb') as f:
+        message = f.read()
+
+def encryptFunc(scheme):
+    global message
+    key = '0123456789abcdef'
+    if scheme == "AES":
+        IV = 16 * '\x00'
+        text2 = '\0' * (16-len(message)%16)
+        text = message + text2    
+        encryptor = AES.new(key, AES.MODE_CBC, IV=IV)
+    elif scheme == "DES3":
+        IV = 8 * '\x00'          
+        text2 = '\0' * (8-len(message)%8)
+        text = message + text2    
+        encryptor = DES3.new(key, DES3.MODE_CBC, IV=IV)
+    elif scheme == "ARC4":
+        IV = 8 * '\x00'          
+        text = message 
+        encryptor = ARC4.new(key)
+    message = text
+    return encryptor.encrypt
+
+def decryptFunc(scheme):
+    global message
+    key = '0123456789abcdef'
+    if scheme == "AES":
+        IV = 16 * '\x00'
+        decryptor = AES.new(key, AES.MODE_CBC, IV=IV)
+    elif scheme == "DES3":
+        IV = 8 * '\x00'
+        decryptor = DES3.new(key, DES3.MODE_CBC, IV=IV)
+    elif scheme == "ARC4":
+        IV = 8 * '\x00'
+        message = message
+        decryptor = ARC4.new(key)
+    return decryptor.decrypt
+
+from multiprocessing import Process, Lock, Pipe
+def execFunc(function, message, locklock, lock, conn):
+    lock.acquire()
+    locklock.release()
+    message = function(message)
+    lock.release()
+    conn.send(message)
+    conn.close()
+
+def monitor(function, outFile):
+    global message
+    startTime = int(time.time() * 1000)
+    parent_conn, child_conn = Pipe()
+    locklock = Lock()
+    locklock.acquire()
+    lock = Lock()
+    p = Process(target=execFunc, args=(function, message, locklock, lock, child_conn))
+    p.start()
+    locklock.acquire()
+    with open(outFile, 'w') as f:
+        while not lock.acquire(block=False):
+            output = '{} {}'.format(int(time.time() * 1000) - startTime, subprocess.check_output("ps --no-headers -o '%cpu,%mem' -p {}".format(p.pid), shell=True).strip())
+            print(output)
+            f.write(output + '\n')
+            time.sleep(.01)
+    message = parent_conn.recv()
+    p.join()
+
+
+pi = 'pi2'
+OS = 'raspbian'
+schemes = ['AES', 'DES3', 'ARC4']
+files = ['foo', 'bar']
+outDir = 'results'
+
+# Ensure outDir exists
+subprocess.call('mkdir {}'.format(outDir), shell=True)
+
+for scheme in schemes:
+    for f in files:
+        print('Reading {}'.format(f))
+        read(f)
+        print('Encrypting {} using {} on a {} running {}'.format(f, scheme, pi, OS))
+        monitor(encryptFunc(scheme), outDir + '/{}-{}-{}-{}-{}.dat'.format(pi, OS, scheme, f, 'encrypt'))
+        print('Decrypting {} using {} on a {} running {}'.format(f, scheme, pi, OS))
+        monitor(decryptFunc(scheme), outDir + '/{}-{}-{}-{}-{}.dat'.format(pi, OS, scheme, f, 'decrypt'))
--- files.sh
+++ files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 declare -a var
-var=( 2000 3000 4000 5000 8000 10000 15000 20000 30000 35000 40000 50000 60000 70000 80000 90000 10000 )
+var=( 2 3 4 5 8 10 15 20 30 35 40 50 60 70 80 90 100 ) # Values in M
+
+size() {
+	mb=$1
+	echo $(expr $mb \* 1024)
+}
 
 for i in "${var[@]}"
 # for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 do 
-#	dd if=/dev/zero of=${var[$i]}  bs=${var[$i]} count=1
-	dd if=/dev/zero of=$i bs=$i count=1
+	#dd if=/dev/zero of=${var[$i]}  bs=${var[$i]} count=1
+	dd if=/dev/urandom of=${i}MB bs=$(size $i) count=1024
 done
 exit 0
