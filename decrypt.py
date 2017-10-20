17518595917a14747c820eb1a6ddfa65c0ca5d6b
akosichesca <akosichesca@gmail.com>
2017-10-20 22:37:04 +0800

    Different Security
    Different Security

--- decrypt.py
+++ decrypt.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python2
 import sys
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, DES3, ARC4
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], 'rb') as f:
     message = f.read()
 ######## Cryptography  ########
 print "Decrypting"
 key = '0123456789abcdef'
-IV = 16 * '\x00'        
-decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+if sys.argv[2] == "AES":
+    IV = 16 * '\x00'        
+    decryptor = AES.new(key, AES.MODE_CBC, IV=IV)
+elif sys.argv[2] == "DES3":
+    IV = 8 * '\x00'          
+    decryptor = DES3.new(key, DES3.MODE_CBC, IV=IV)
+elif sys.argv[2] == "ARC4":
+    IV = 8 * '\x00'          
+    message = message 
+    decryptor = ARC4.new(key)
+else:
+    IV = 16 * '\x00'  
+    decryptor = AES.new(key, AES.MODE_CBC, IV=IV)
+
+
+
 message = decryptor.decrypt(message)
 print "Writing"
-with open('output', 'w') as f:
+with open('output', 'wb') as f:
     f.write(message)
 ###############################
--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python2
 import sys
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, DES3, ARC4
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], 'rb') as f:
     message = f.read()
 ######## Cryptography  ########
 print "Encrypting"
 key = '0123456789abcdef'
-IV = 16 * '\x00'          
-mode = AES.MODE_CBC
-encryptor = AES.new(key, mode, IV=IV)
-text2 = '\0' * (16-len(message)%16)
-text = message + text2
+
+
+if sys.argv[2] == "AES":
+    IV = 16 * '\x00'        
+    text2 = '\0' * (16-len(message)%16)
+    text = message + text2    
+    encryptor = AES.new(key, AES.MODE_CBC, IV=IV)
+elif sys.argv[2] == "DES3":
+    IV = 8 * '\x00'          
+    text2 = '\0' * (8-len(message)%8)
+    text = message + text2    
+    encryptor = DES3.new(key, DES3.MODE_CBC, IV=IV)
+elif sys.argv[2] == "ARC4":
+    IV = 8 * '\x00'          
+    text = message 
+    encryptor = ARC4.new(key)
+else:
+    IV = 16 * '\x00'  
+    text2 = '\0' * (16-len(message)%16)
+    text = message + text2       
+    encryptor = AES.new(key, AES.MODE_CBC, IV=IV)
+
+
 message = encryptor.encrypt(text)
+
+
+
+
 print "Writing"
-with open('output', 'w') as f:
+with open('output', 'wb') as f:
     f.write(message)
 ###############################
